6c1a9ac8e464f8ff7be6d57a23a3a33369310d9b
dgregorio <dgregorio.libunao@gmail.com>
2016-07-24 20:25:37 +0800

    Code Updates
    Code updates and clean-up.

--- SelectTransactionType.java
+++ SelectTransactionType.java
@@ -8,20 +8,23 @@ import javax.swing.*;
  Label la=new Label("Select Transaction Type:");
  Button bt=new Button("Next");
 
-
  fm.setLayout(new GridLayout(0, 1));
  CheckboxGroup cg1=new CheckboxGroup();
- CheckboxGroup cg2=new CheckboxGroup();
+
  fm.add(la);
  fm.add(new Checkbox("Reload", cg1, true));
  fm.add(new Checkbox("Smart Padala", cg1, false));
+
  fm.add(bt);
+
  fm.setSize(300,300);
  fm.setVisible(true);
  fm.addWindowListener(new WindowAdapter(){
+
  public void windowClosing(WindowEvent we){
  System.exit(0);
  }
  });
+
  }
 }
