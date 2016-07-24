385830fffcfb3a734f2b7bd0821f48075f65d4c7
dgregorio <dgregorio.libunao@gmail.com>
2016-07-24 20:11:14 +0800

    Added SelectTransactionType.java
    Added codes snippet for selecting transaction type.

---
+++ SelectTransactionType.java
@@ -0,0 +1,27 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+ public class SelectTransactionType{
+ public static void main(String[] args) {
+ Frame fm=new Frame("Transaction Type");
+ Label la=new Label("Select Transaction Type:");
+ Button bt=new Button("Next");
+
+
+ fm.setLayout(new GridLayout(0, 1));
+ CheckboxGroup cg1=new CheckboxGroup();
+ CheckboxGroup cg2=new CheckboxGroup();
+ fm.add(la);
+ fm.add(new Checkbox("Reload", cg1, true));
+ fm.add(new Checkbox("Smart Padala", cg1, false));
+ fm.add(bt);
+ fm.setSize(300,300);
+ fm.setVisible(true);
+ fm.addWindowListener(new WindowAdapter(){
+ public void windowClosing(WindowEvent we){
+ System.exit(0);
+ }
+ });
+ }
+}
